42d86a73f65158be7062ebb8000453a1129ac5e0
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-20 14:56:06 -0700

    Modal for adding books completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 // in terminal 'yarn add axios' then import it
 import axios from "axios";
-import { Table, Button } from "reactstrap";
+import {
+  Table,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
 class App extends Component {
   state = {
-    books: []
+    books: [],
+    newBookData: {
+      title: "",
+      rating: ""
+    },
+    newBookModal: false
   };
 
   componentWillMount() {
@@ -14,6 +29,21 @@ class App extends Component {
         books: response.data
       });
     });
+  }
+
+  toggleNewBookModal() {
+    this.setState({
+      newBookModal: !this.state.newBookModal
+    });
+    // this.state.newBookModal = true;
+  }
+
+  addBook() {
+    axios
+      .post("http://localhost:3000/books", this.state.newBookData)
+      .then(response => {
+        console.log(response.data);
+      });
   }
 
   render() {
@@ -37,6 +67,55 @@ class App extends Component {
 
     return (
       <div className="App container">
+        <Button color="primary" onClick={this.toggleNewBookModal.bind(this)}>
+          Add Book
+        </Button>
+        <Modal
+          isOpen={this.state.newBookModal}
+          toggle={this.toggleNewBookModal.bind(this)}
+        >
+          <ModalHeader toggle={this.toggleNewBookModal.bind(this)}>
+            Add a new book
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input
+                id="title"
+                value={this.state.newBookData.title}
+                onChange={e => {
+                  let { newBookData } = this.state;
+                  newBookData.title = e.target.value;
+
+                  this.setState({ newBookData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="rating">Rating</Label>
+              <Input
+                id="rating"
+                value={this.state.newBookData.rating}
+                onChange={e => {
+                  let { newBookData } = this.state;
+                  newBookData.rating = e.target.value;
+                  this.setState({ newBookData });
+                }}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.addBook.bind(this)}>
+              Add Book
+            </Button>{" "}
+            <Button
+              color="secondary"
+              onClick={this.toggleNewBookModal.bind(this)}
+            >
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
         <Table>
           <thead>
             <tr>
